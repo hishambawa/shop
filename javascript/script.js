731e6559c881828149ce67b99c5df6005f8823af
Hisham Bawa <hishambawa@icloud.com>
2021-05-30 19:52:56 +0530

    Header and Sidebar Done

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+function menu(){
+
+    let menu = document.getElementById("navigation");
+
+    if(menu.style.display == "block"){
+        menu.style.display = "none";
+    }
+
+    else{
+        menu.style.display = "block";
+    }
+}
+
+function filter(){
+
+    let filter = document.getElementById("filter");
+
+    if(filter.style.display == "block"){
+        filter.style.display = "none";
+    }
+
+    else{
+        filter.style.display = "block";
+    }
+
+   
+}
